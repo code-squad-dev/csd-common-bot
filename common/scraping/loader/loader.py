4d3e787c4111ad7c4ccc37c31e37c019c08e9f54
dpancu <dpancu@godaddy.com>
2020-10-28 19:36:23 +0200

    Added get soup of page by tag, added new dependencies

--- common/scraping/loader/loader.py
+++ common/scraping/loader/loader.py
@@ -5,6 +5,8 @@ from selenium.webdriver.phantomjs.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+from common.scraping.collector import extractor
+
 
 def get_page_source_until_all_tags(driver: WebDriver, tag: str, timeout: int = 10):
     WebDriverWait(driver, timeout).until(EC.presence_of_all_elements_located((By.TAG_NAME, tag)))
@@ -37,3 +39,18 @@ def get_page_source_until_all_selectors_with_delay(driver: WebDriver, selector: 
     WebDriverWait(driver, timeout).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, selector)))
     time.sleep(delay)
     return driver.page_source
+
+
+def load_page_soup_by_all_tags_with_delay(driver: WebDriver, tag: str):
+    page_content = get_page_source_until_all_tags_with_delay(driver=driver, tag=tag)
+    return extractor.get_soup_by_content(page_content)
+
+
+def load_page_soup_by_all_selectors_with_delay(driver: WebDriver, selector: str):
+    page_content = get_page_source_until_all_selectors_with_delay(driver=driver, selector=selector)
+    return extractor.get_soup_by_content(page_content)
+
+
+def load_page_soup_by_all_names_with_delay(driver: WebDriver, name: str):
+    page_content = get_page_source_until_all_names_with_delay(driver=driver, name=name)
+    return extractor.get_soup_by_content(page_content)
